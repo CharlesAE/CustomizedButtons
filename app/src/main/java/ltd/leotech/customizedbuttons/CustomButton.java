70de11d05045dba029e31e11f1835f570d4a4ea5
Charles E <supa.ram.studios@gmail.com>
2017-10-26 16:50:29 -0400

    Update CustomButton.java

--- app/src/main/java/ltd/leotech/customizedbuttons/CustomButton.java
+++ app/src/main/java/ltd/leotech/customizedbuttons/CustomButton.java
@@ -6,7 +6,7 @@ import android.util.AttributeSet;
 import android.widget.Button;
 
 /**
- * Created by suparam on 10/26/17.
+ * Created by CharlesAE on 10/26/17.
  */
 
 public class CustomButton extends android.support.v7.widget.AppCompatButton {
